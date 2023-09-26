bb3e5332c2e3eb97de398847bc080c57045c688a
tony17b <tony.barbier@oclock.school>
2023-09-26 22:17:44 +0200

    dernier essai avant le dodo

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -17,41 +17,51 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
-    /**
-     * GET users collection
-     * 
-     * @Route("/api/user", name="app_api_user", methods={"GET"})
-     */
-    public function getUserAll(UserRepository $userRepository): JsonResponse
-    {
-        $users = $userRepository->findAll();
-        
-        return $this->json(
-            $users,
-            200,
-            [],
-            ['groups' => 'get_login']
-        );
-    } 
-
-    /**
-     * GET users collection
-     *
-     * @Route("/api/user/{id}", name="app_api_user_by_id", methods={"GET"})
-     */
-    public function getUserById(UserRepository $userRepository, $userId): JsonResponse
-    {
-        $user = $userRepository->findBy(['id' => $userId]);
-
+/**
+ * GET users collection
+ * 
+ * @Route("/api/user", name="app_api_user", methods={"GET"})
+ */
+public function index(UserRepository $userRepository)
+{
+    $users = $userRepository->findAll();
     
-        return $this->json(
-        
-            $user,
-            200,
-            [],
-            ['groups' => 'get_id']
-    
-        );
+    // Vous pouvez ajuster ici les données renvoyées selon vos besoins.
+    // Par exemple, si vous souhaitez inclure uniquement certaines propriétés de chaque utilisateur.
+    $data = [];
+    foreach ($users as $user) {
+        $data[] = [
+            'id' => $user->getId(),
+            'username' => $user->getUsername(),
+            // Ajoutez d'autres propriétés ici selon vos besoins.
+        ];
     }
 
+    return $this->json($data, 200, [], ['groups' => 'get_login']);
+} 
+
+/**
+ * GET users collection
+ *
+ * @Route("/api/user/{id}", name="app_api_user_by_id", methods={"GET"})
+ */
+public function getUserById(UserRepository $userRepository, $id): JsonResponse
+{
+    $user = $userRepository->find($id);
+
+    if (!$user) {
+        // L'utilisateur avec l'ID donné n'a pas été trouvé
+        return $this->json(['error' => 'User not found'], 404);
+    }
+
+    // Vous pouvez ajuster ici les données renvoyées selon vos besoins.
+    // Par exemple, si vous souhaitez inclure uniquement certaines propriétés de l'utilisateur.
+    $data = [
+        'id' => $user->getId(),
+        'username' => $user->getUsername(),
+        // Ajoutez d'autres propriétés ici selon vos besoins.
+    ];
+
+    return $this->json($data, 200, [], ['groups' => 'get_login']);
+}
 }
