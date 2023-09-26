98df64a49ea80c234ae52f34b48fd5576fb3bdf5
tony17b <tony.barbier@oclock.school>
2023-09-26 08:33:39 +0200

    include controller

---
+++ src/Controller/RoundController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RoundController extends AbstractController
+{
+    /**
+     * @Route("/round", name="app_round")
+     */
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/RoundController.php',
+        ]);
+    }
+}
