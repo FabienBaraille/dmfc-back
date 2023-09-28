4fec6961e7de31b5272b222bccd2c7bba73b54b3
Maxime-Lemarchand <maxime.lemarchand.13@gmail.com>
2023-09-28 16:27:41 +0200

    Correction fixture bdd, merge

--- migrations/Version20230928140136.php
+++ migrations/Version20230928140136.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230927122305 extends AbstractMigration
+final class Version20230928140136 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20230927122305 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649F85E0677 ON user (username)');
+        $this->addSql('ALTER TABLE user CHANGE role roles LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json)\'');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP INDEX UNIQ_8D93D649F85E0677 ON user');
+        $this->addSql('ALTER TABLE user CHANGE roles role LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json)\'');
     }
 }
