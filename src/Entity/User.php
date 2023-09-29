d38b98b2f219e226a549b687e9003aa8a5ff022e
Maxime-Lemarchand <maxime.lemarchand.13@gmail.com>
2023-09-29 09:40:28 +0200

    Token authorisation by road

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -184,7 +184,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): ?array
     {
         $roles = $this->roles;
-        $roles[] = 'ROLE_JOUEUR';
+        // $roles[] = 'ROLE_JOUEUR';
 
         return array_unique($roles);
     }
