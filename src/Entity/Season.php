a6c9093137ac42fda3d38aa616dbc0733d8d64be
Maxime-Lemarchand <maxime.lemarchand.13@gmail.com>
2023-10-03 14:51:59 +0200

    modification doublon

--- src/Entity/Season.php
+++ src/Entity/Season.php
@@ -8,8 +8,6 @@ use App\Entity\Round;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Doctrine\Common\Collections\ArrayCollection;
-use Symfony\Component\Serializer\Annotation\Groups;
 
 
 /**
