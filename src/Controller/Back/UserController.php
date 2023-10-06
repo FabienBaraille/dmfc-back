c1b53d3d18eabe7847169cbe26b0d29174a0bf90
Maxime-Lemarchand <maxime.lemarchand.13@gmail.com>
2023-10-06 09:10:31 +0200

    Back\UserConttroller function show by Id

--- src/Controller/Back/UserController.php
+++ src/Controller/Back/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Back;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,5 +18,15 @@ class UserController extends AbstractController
         return $this->render('back/user/index.html.twig', [
             'users' => $userRepository->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/back/user/{id}", name="app_back_user_show", methods={"GET"})
+     */
+    public function show(User $user): Response
+    {
+    return $this->render('back/user/show.html.twig', [
+    'user' => $user,
+    ]);
     }
 }
