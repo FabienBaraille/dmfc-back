7ed626d414daac032a6fc5f87f8576a62597a41e
FabienBaraille <fabien.baraille@oclock.school>
2023-11-16 17:09:32 +0100

    DMFC creation, Get game by round change, new relation

--- src/Entity/Selection.php
+++ src/Entity/Selection.php
@@ -27,6 +27,16 @@ class Selection
      */
     private $selectedHome;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=Team::class, inversedBy="selections")
+     */
+    private $teams;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=League::class, inversedBy="selections")
+     */
+    private $leagues;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -52,6 +62,30 @@ class Selection
     public function setSelectedHome(?int $selectedHome): self
     {
         $this->selectedHome = $selectedHome;
+
+        return $this;
+    }
+
+    public function getTeams(): ?Team
+    {
+        return $this->teams;
+    }
+
+    public function setTeams(?Team $teams): self
+    {
+        $this->teams = $teams;
+
+        return $this;
+    }
+
+    public function getLeagues(): ?League
+    {
+        return $this->leagues;
+    }
+
+    public function setLeagues(?League $leagues): self
+    {
+        $this->leagues = $leagues;
 
         return $this;
     }
